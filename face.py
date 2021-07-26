3c21bf397c8f79a0653febc355b44e3bb31d73a7
Parag <paragdurafe@outlook.com>
2021-07-27 01:22:41 +0530

    Faces Added

--- face.py
+++ face.py
@@ -1,1 +1,12 @@
 import cv2
+face_file=cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+img=cv2.imread("img2.jpg")
+
+face=face_file.detectMultiScale(img,scaleFactor=1.05,minNeighbors=5)
+
+for x,y,w,h in face:
+    img=cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),3)
+
+cv2.imshow('Face Detect',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
