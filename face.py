c958d15369773af3ebd7edfe0d94d6871b691a25
Parag <paragdurafe@outlook.com>
2021-07-28 01:26:41 +0530

    Grayscale img

--- face.py
+++ face.py
@@ -1,11 +1,12 @@
 import cv2
 face_file=cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
-img=cv2.imread("Images/img2.jpg")
+img=cv2.imread("Images/img4.png")
+gray_scale_img=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
-face=face_file.detectMultiScale(img,scaleFactor=1.05,minNeighbors=5)
+face=face_file.detectMultiScale(gray_scale_img,scaleFactor=1.05,minNeighbors=4)
 
 for x,y,w,h in face:
-    img=cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),3)
+    img=cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
 
 cv2.imshow('Face Detect',img)
 cv2.waitKey(0)
